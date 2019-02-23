5365f37e6543365eb5fb397614523fbe41ddd7ff
fengshuan <1263215592@qq.com>
2019-02-23 21:05:06 +0800

    perf:服务端渲染代码优化

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import { render } from './utils';
+import routes from '../Routes';
+import getStore from '../store';
+import { matchRoutes } from 'react-router-config';
+
 // 客户端渲染
 // react代码在浏览器上执行，消耗的是用户浏览器的性能
 
@@ -10,7 +14,22 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-    render(req, res);
+    const store = getStore();
+
+    // 根据路由的路径，来往store里面加数据
+    const matchedRoutes = matchRoutes(routes, req.path);
+
+    // 让matchedRoutes里面所有的组件，对应的loadData方法执行一次
+    const promises = [];
+    matchedRoutes.forEach(item => {
+        if(item.route.loadData) {
+            promises.push(item.route.loadData(store))
+        }
+    })
+
+    Promise.all(promises).then(() => {
+        res.send(render(store, routes, req))
+    })
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- src/server/utils.js
+++ src/server/utils.js
@@ -1,28 +1,10 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter, Route } from 'react-router-dom';
-import { matchRoutes } from 'react-router-config';
-import routes from '../Routes';
 import { Provider } from 'react-redux';
-import getStore from '../store';
 
 
-export const render = (req, res) => {
-
-    const store = getStore();
-
-    // 根据路由的路径，来往store里面加数据
-    const matchedRoutes = matchRoutes(routes, req.path);
-
-    // 让matchedRoutes里面所有的组件，对应的loadData方法执行一次
-    const promises = [];
-    matchedRoutes.forEach(item => {
-        if(item.route.loadData) {
-            promises.push(item.route.loadData(store))
-        }
-    })
-
-    Promise.all(promises).then(() => {
+export const render = (store, routes, req) => {
         const content = renderToString((
             <Provider store={store}>
                 <StaticRouter location={req.path} context={{}}>
@@ -35,7 +17,7 @@ export const render = (req, res) => {
             </Provider>   
         ));
     
-        res.send(`<html>
+        return `<html>
             <head>
                 <title>ssr</title>
             </head>
@@ -43,6 +25,5 @@ export const render = (req, res) => {
                 <div id="root">${content}</div>
                 <script src='/index.js'></script>
             </body>
-        </html>`);  
-    })
+        </html>`;  
 }
